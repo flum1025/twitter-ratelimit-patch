73b842379a6c200eaa79e5b72d63e684bcd27891
flum1025 <flum.1025@gmail.com>
2017-11-23 03:11:52 +0900

    first commit

---
+++ lib/twitter/ratelimit/patch.rb
@@ -0,0 +1,9 @@
+require "twitter/ratelimit/patch/version"
+
+module Twitter
+  module Ratelimit
+    module Patch
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/twitter/ratelimit/patch/version.rb
@@ -0,0 +1,7 @@
+module Twitter
+  module Ratelimit
+    module Patch
+      VERSION = "0.1.0"
+    end
+  end
+end
