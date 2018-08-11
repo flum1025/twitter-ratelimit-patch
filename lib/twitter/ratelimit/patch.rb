b0165d41eb12773634a1bac85274079075106d09
flum1025 <flum.1025@gmail.com>
2018-08-11 20:18:29 +0900

    for home_timeline

--- lib/twitter/ratelimit/patch.rb
+++ lib/twitter/ratelimit/patch.rb
@@ -12,6 +12,23 @@ module Twitter::REST::Request::Extend
   end
 end
 
+module Twitter
+  module REST
+    module Utils
+      def perform_request_with_objects(request_method, path, options, klass)
+        pr = perform_request(request_method, path, options)
+        collected = pr.collect do |element|
+          klass.new(element)
+        end
+        collected.define_singleton_method :rate_limit do
+          pr.rate_limit
+        end
+        collected
+      end
+    end
+  end
+end
+
 class Twitter::Base
   def rate_limit
     attrs.rate_limit
